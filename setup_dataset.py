1dbe040ea52ead1f3fa56ad856ebf775fd527cfe
Adel Hasic <hasic@student.chalmers.se>
2022-09-09 14:59:03 +0200

    Add argparser to `setup_dataset.py`

--- setup_dataset.py
+++ setup_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import zipfile
 
@@ -8,11 +9,19 @@ assert os.getcwd().endswith('/dl4de')
 # Set paths accordingly if changed from default
 DOWNLOAD_LOCATION = './tmp/downloads/'
 DATASETS_LOCATION = './datasets/'
-FILENAME = 'data_semantics.zip'
-# TODO: add arg-parsing for dataset selection
 
-assert os.path.isfile(DOWNLOAD_LOCATION + FILENAME)
+DATASET_FILE_NAMES = {
+    'semantics': "data_semantics.zip",
+    'sceneflow': "data_scene_flow.zip",
+}
 
-with zipfile.ZipFile(DOWNLOAD_LOCATION + FILENAME) as archive:
-    # archive.printdir()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'dataset', help='select dataset', choices=['semantics', 'sceneflow'])
+args = parser.parse_args()
+
+file_path = DOWNLOAD_LOCATION + DATASET_FILE_NAMES[args.dataset]
+assert os.path.isfile(file_path)
+
+with zipfile.ZipFile(file_path) as archive:
     archive.extractall(path=DATASETS_LOCATION + 'kitti')
