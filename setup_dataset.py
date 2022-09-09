5ad8ef5f5f1962914de02369056612f28f31bcdc
Adel Hasic <hasic@student.chalmers.se>
2022-09-09 17:32:21 +0200

    Limit extracted data to relevant subsets only

--- setup_dataset.py
+++ setup_dataset.py
@@ -15,6 +15,11 @@ DATASET_FILE_NAMES = {
     'sceneflow': "data_scene_flow.zip",
 }
 
+DATASET_SUBSETS = [
+    'image_2', 'instance', 'semantic',
+    'image_3', 'disp_occ_0', 'disp_noc_0',
+]
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'dataset', help='select dataset', choices=['semantics', 'sceneflow'])
@@ -24,4 +29,7 @@ file_path = DOWNLOAD_LOCATION + DATASET_FILE_NAMES[args.dataset]
 assert os.path.isfile(file_path)
 
 with zipfile.ZipFile(file_path) as archive:
-    archive.extractall(path=DATASETS_LOCATION + 'kitti')
+    for filename in archive.namelist():
+        category = filename.split('/')[1]
+        if category in DATASET_SUBSETS and filename.endswith('10.png'):
+            archive.extract(filename, DATASETS_LOCATION + 'kitti')
